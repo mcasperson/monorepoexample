14420e07f1009586a8ef9387d6d71bdf6c8a10f2
Matthew Casperson <matthewcasperson@gmail.com>
2025-02-24 09:04:34 +1000

    Testing filtered build information action

---
+++ frontend/server.js
@@ -0,0 +1,14 @@
+const { createServer } = require('node:http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>Frontend</h1></body></html>');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
