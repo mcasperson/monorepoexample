4f1abc0b249705d206749f95a098ebf5fb917515
Matthew Casperson <matthewcasperson@gmail.com>
2025-02-24 09:57:31 +1000

    Get all commits

--- frontend/server.js
+++ frontend/server.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>Frontend edited 5</h1></body></html>');
+    res.end('<html><body><h1>Frontend edited 6</h1></body></html>');
 });
 
 server.listen(port, hostname, () => {
