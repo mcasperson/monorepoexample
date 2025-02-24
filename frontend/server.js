569f3d304ec71e010c659e0484de179d33be895f
Matthew Casperson <matthewcasperson@gmail.com>
2025-02-24 12:49:38 +1000

    updated frontend

--- frontend/server.js
+++ frontend/server.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>Frontend edited 11</h1></body></html>');
+    res.end('<html><body><h1>Frontend edited 12</h1></body></html>');
 });
 
 server.listen(port, hostname, () => {
